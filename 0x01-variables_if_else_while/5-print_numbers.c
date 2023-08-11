6f7140e991d2125a211371b20885dbb4d3155abc
Alexander Theuri <lextheuri90@gmail.com>
2023-08-11 12:08:24 -0700

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * Description: prints base 10 numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+putchar('\n');
+return (0);
+}
